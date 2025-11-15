84dd9107557829dd7b6eb6f5b699444cc3ae3e0e
Nicholas <nicholas@tinkertank.rocks>
2025-11-16 07:36:47 +1100

    Fix Stripe API version in test script

--- scripts/test-stripe-payment.ts
+++ scripts/test-stripe-payment.ts
@@ -18,7 +18,7 @@ if (!STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
 
 async function createTestPayment() {
